38aa82303ef1403179fca5ba25ce7f02f7ea3b9c
Vitalii Tymofiichuk <vetalmoff@gmail.com>
2021-03-16 14:22:45 +0200

    add first name and last name to devise registration and edit views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   enum role: { member: 0, moderator: 1, admin: 2 }, _default: 0
+  validates :first_name, :last_name, :email, :password, :password_confirmation, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
