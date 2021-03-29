609983f3fb9ac97341dbd235c8e3694af32a9079
Vitalii Tymofiichuk <vetalmoff@gmail.com>
2021-03-29 12:55:27 +0300

    added admin namespace

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
     before_action :configure_permitted_parameters, if: :devise_controller?
 
-    helper_method :is_admin!
+    # helper_method :is_admin!
 
     protected
 
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
         devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :email, :password])
     end
 
-    def is_admin!
-        redirect_to root_path unless current_user&.admin?
-    end
+    # def is_admin!
+    #     redirect_to root_path unless current_user&.admin?
+    # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts, only: [:show, :index]
+
   root 'posts#index'
+
+  namespace :admin do
+    resources :posts 
+  end
+
   devise_for :users
+
   # get 'pages/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   scope module: 'pages' do
